c8c30445b52081c85fd1c51e15494a080ccbbde1
ericludfi19 <ericludfi19@gmail.com>
2024-11-07 03:27:14 +0000

    commit new

--- lib/app/modules/mahasiswa/views/mahasiswa_view.dart
+++ lib/app/modules/mahasiswa/views/mahasiswa_view.dart
@@ -12,7 +12,31 @@ class MahasiswaView extends GetView<MahasiswaController> {
     return StreamBuilder<QuerySnapshot<Object?>>(
         stream: Get.put(MahasiswaController()).StreamData(),
         builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.active) {}
+          if (snapshot.connectionState == ConnectionState.active) {
+            // mengambil data
+            var listAllDocs = snapshot.data?.docs ?? [];
+            return listAllDocs.length > 0
+                ? ListView.builder(
+                    itemCount: listAllDocs.length,
+                    itemBuilder: (context, index) => ListTile(
+                      leading: CircleAvatar(
+                        child: Text('${index + 1}'),
+                        backgroundColor: Color.fromARGB(255, 248, 248, 248),
+                      ),
+                      title: Text(
+                          "${(listAllDocs[index].data() as Map<String, dynamic>)["nama"]}"),
+                      subtitle: Text(
+                          "${(listAllDocs[index].data() as Map<String, dynamic>)["npm"]}"),
+                      trailing: IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.more_vert),
+                      ),
+                    ),
+                  )
+                : Center(
+                    child: Text("Data Kosong"),
+                  );
+          }
           return Center(
             child: CircularProgressIndicator(),
           );
